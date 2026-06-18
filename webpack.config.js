f63184eb5cb4224d0416cf35765bedf55264868f
EvgenKor <evgen2k7@yandex.ru>
2026-06-18 13:18:52 +0300

    Updating library stack, tuning config

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,12 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.m?js$/,
+                resolve: {
+                    fullySpecified: false,
+                },
+            },
+            {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
             },
